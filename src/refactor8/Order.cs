387fc9ecba271677b4db0e4053c523ed75bf50bf
Wang Jian <jianwang@thoughtworks.com>
2014-03-02 17:57:02 +0800

    init project.

---
+++ src/refactor8/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace refactor8
+{
+    public class Order
+    {
+        public Order(string customName)
+        {
+            Customer = customName;
+        }
+
+        public string Customer { get; set; }
+    }
+}
---
+++ test/refactor8_test/OrderFacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using refactor8;
+
+namespace refactor8_test
+{
+    public class OrderFacts
+    {
+        [Fact]
+        public void should_get_name()
+        {
+            IEnumerable<Order> orders = new List<Order>
+                {
+                    new Order("1"),
+                    new Order("2")
+                };
+            Assert.Equal(1, NumberOfOrdersFor(orders, "1"));
+        }
+
+        public static int NumberOfOrdersFor(IEnumerable<Order> orders, string customer)
+        {
+            return orders.Count(o => o.Customer == customer);
+        }
+    }
+}
